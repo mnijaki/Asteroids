0f4e74bcbf32feb3e381c3d9f69fc421c803276e
mnijaki <mnijaki@gmail.com>
2018-09-29 00:46:55 +0200

    Added Readme

---
+++ Assets/General/Scripts/Readme.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+// Readme.
+public class Readme: MonoBehaviour
+{
+  // ---------------------------------------------------------------------------------------------------------------------
+  // Only for purpose of readme                  
+  // ---------------------------------------------------------------------------------------------------------------------
+  // •  Find 'TO_DO' and 'Debug'
+  // •  Pomyslec nad przerobieniem ruchu statkow na takie jak w 'laserDefender' - za pomoca waypointow
+  // •  Przerob strzelanie na takie jak Rick uzyl (chyba dzieki temu bedzie mozna czesciej strzelac gdy bedzie sie 
+  //    samemu wciskac klawisz)
+  // •	Dodać poruszanie się kamery podczas ruchu gracza, tak by miało się wrażenie większej głębi
+  // •	Skonfigurować ‘PostProcessing’
+  // •	Dodać ikonę gry
+  // •	Dodać obsługę na komórki (z użyciem 'CrossPlatformInput')
+  // •	Poprawić 'boundry' na 'boundary'
+  // •	Dodać 'pooling' obiektów (np. pocisków oraz przeciwników)
+  // •	Pomyśleć nad użyciem 'Cinemamachine'
+  // •	Dopracować 'WinScreen' oraz 'LoseScreen'
+  // •	Dodać obsługę 'PauseMenu'
+  // •	Poprawić oświetlenie
+  // •	Dodać obsługę ciężaru broni (im większy ciężar, tym wolniej samolot gracza się porusza)
+  // •	Dodać teleporty
+  // •	Dodać lądowiska z paliwem i obsługę paliwa w statku gracza (zmienić położenie gracza w przestrzeni 'y'  
+  //    przy lądowaniu na stacji)
+  // •	Dodać włączanie grawitacji na samolocie gracza, gdy skończy mu się paliwo
+  // •	Dodać 'PowerUp'-y 
+  // •	Dodać obsługę bocznych 'spawnerów'
+  // •	Zmienić 'FOV' i 'Depth' głównej kamery przy przejściu do następnego poziomu
+  // •	Zmienić prędkość przewijania tła oraz rozciągnąć tło przy przejściu do następnego poziomu
+  // •	Dodać efekt 'SlowMotion' gdy statek gracza przelatuje blisko np. asteroidy
+  // •	Dodać obsługę poziomu trudności
+  // •	Poprawić logikę 'spawnowania' przeciwników
+  // •	Pomyśleć nad rozwiązaniem problemu przenikania się obiektów (różne kamery z różnymi poziomami sortowania lub 
+  //    odpowiednio napisany shader)
+  // •	Ukrywać/pokazywać kursor
+  // •	Dodać efekt rozbłysku przy strzelaniu 
+  // •	Dodać poziomy z widokiem zza pleców
+  // •	Dodać wyświetlanie broni oraz dodatków na samolocie gracza
+  // •	Dodać poziom gdzie gracz może strzelać tylko jednym pociskiem (maksymalnie 1 aktywny pocisk na ekranie)
+  // •	Dodać animacje twarzy pilota w HUD
+  // •	Dodać asteroidy rozpadające się na mniejsze kawałki
+  // •	Dodać nieśmiertelność przez 5s. po odrodzeniu się gracza na planszy
+  // •	Dodać pociski samonaprowadzające z tarczą aktywowaną na prawy przycisk myszy
+  // •	Dodać komety i planety w tle
+  // •	Wysokość położenia samolotu gracza w pionie ('z') powinna wpływać na szybkość rozgrywki
+  // •	Dodać asteroidy z działkami oraz asteroidy obrońcy w walce z bossami
+  // •	Dodać bossów
+
+
+} // End of Readme
